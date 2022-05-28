a701c5632f853022753c756d18a38698a3807014
Each Chen <98824625+iach526526@users.noreply.github.com>
2022-05-28 13:24:30 +0800

    Update popcat.py

--- popcat.py
+++ popcat.py
@@ -1,16 +1,11 @@
 from selenium import webdriver
-#from selenium.webdriver.common.action_chains import ActionChains
-import time
-path="D:/chromedriver_win32/chromedriver.exe"
+path="D:/chromedriver_win32/chromedriver.exe"#放chromedriver的檔案路徑
 
 
 driver=webdriver.Chrome(path)
 driver.get("https://popcat.click/")
 
-# blow=driver.find.el
 
-# action=ActionChains(driver)
-# action.click()
 c=driver.find_element_by_xpath('//*[@id="app"]/div')
 while True:
     c.click()
